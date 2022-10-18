d191578007d7a78053752af0630972639ca6dca2
mohammed-ostath <mohammedostath@gmailcom>
2022-10-18 17:02:39 +0300

    make dashpoard

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,17 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::prefix('admin')->name('admin.')->middleware('auth','check_admin')->group(function () {
+    Route::get('/', [AdminController::class, 'index'])->name('index');
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
